608d16d2cebd3c4f001886363fd395af4a4e7d16
gkurniiawant <gkurniiawant@gmail.com>
2022-02-03 00:09:02 +0700

    Dsadsa

--- new.js
+++ new.js
@@ -29,10 +29,10 @@ var profit = 0;
         } else if (profit == 0) {
             //bet = bet;
         } else {
-            bet = (Math.floor(Math.random() * 10) + 1) / 1000;
+            bet = (Math.floor(Math.random() * 10) + 1) / 10000;
         }
         if (bet > 5) {
-            bet = (Math.floor(Math.random() * 10) + 1) / 1000;
+            bet = (Math.floor(Math.random() * 10) + 1) / 10000;
         }
     }
 })();
