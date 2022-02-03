1369db5c539843cc3fd841e1195ed1a84d009e69
gkurniiawant <gkurniiawant@gmail.com>
2022-02-03 15:49:42 +0700

    Dsadsa

--- seed.php
+++ seed.php
@@ -60,6 +60,7 @@ x-requested-with: XMLHttpRequest';
 function kseed()
 {
     $rseed = hash('sha256', rand(11111111, 99999999) . '' . rand(11111111, 99999999) . '' . rand(11111111, 99999999) . '' . rand(11111111, 99999999) . '' . rand(11111111, 99999999) . '' . rand(11111111, 99999999) . '' . rand(11111111, 99999999) . '' . rand(11111111, 99999999));
+    $rseed = substr($rseed, 0, 64);
     $d['url'] = "https://wolf.bet/api/v1/user/seed/refresh";
     $d['header'] = 'accept-language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7,ru;q=0.6
 authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI1IiwianRpIjoiYWQyYmEwZTRhNDBiOTMxMGJhZTg1ZmQ4ODAyNmE0OTUwMjZmNDE0MWRiMDNkMWNmMzMyNTY5ZmZlMDdjMTE3YzJiMDUyMDczMmFmMzg1NzUiLCJpYXQiOjE2NDM2ODY1MTIsIm5iZiI6MTY0MzY4NjUxMiwiZXhwIjoxNjc1MjIyNTEyLCJzdWIiOiI0MDkwMTMiLCJzY29wZXMiOltdfQ.hz8_cyB_kemH8kfIXy13RxhMdbiw5YzPXaDh3Q70-W7f7LOas9EjA8Z2vDXraB9gWjrTCUnzpM_RVOxEgozzHGk9QnWvjZTlu7j9TYnxaP80kC8STIT4AVRhRfKR6aNUvhuDYb0IHzdja0xn1n4Wo9bxxAlRaMPFGCM7ceBTs-cZd2gWfFLjtaaDM_AM3CjS_rIOhLyjFk8VclaEjvDnBH5H5Zgtf5FSMLhWNzezHSRoIiY-V4BydD262i5tW2XbqPIdArJQwAxEYrQR67PmfJzQ-s1IGjzyG75sR-F7MrdkR00r_JCRRE6xtBAO4-n4Qbk3fJFt7VfArPz9cGkLcaR0ta5eMRcHPhOJow21i4ck2_Q_odcMgDtIX1Uf11I0Z1ILSyvPOd-nS_tSnrKEUcwPoOaNdwgH6-CCPJeylJeHzRtnv_G2W8Tnn6aiXmSzS_5gxX4I7xdzvM4Y7kVAllK99TqSxTQrW1dbXZinq3pyP9TO0ttLEZOLS07mLBFnRbywI9EN29LGAWu04gg6i9llXHIed3kNiRGVv6OIO5iBs1h-ExbhcDyp7puUVvG7cnTIyfuYt8pCAYquh7lFQvY7f2isEPOShhMy8Eepmx7dtQhMTEXDTwagP_1CkZMNQt0FNJ5cXZbLVJLie76DwPHJHhajmDofm_P5CFgv7bo
