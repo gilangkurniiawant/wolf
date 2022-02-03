70829a8927b7991ed0aa168961ef5690f1ebbbb1
gkurniiawant <gkurniiawant@gmail.com>
2022-02-03 17:29:22 +0700

    Dsadsa

--- new.js
+++ new.js
@@ -15,7 +15,7 @@ var headers = {
     'x-hash-api': 'ba6c4afbf4e7264f12097838641d1e9684d2de9840a88581c952afc9a6ee036b',
     'x-requested-with': 'XMLHttpRequest'
 };
-var base_bet = 0.000002;
+var base_bet = 0.0002;
 var bet = base_bet;
 var profit = 0;
 (async() => {
@@ -73,7 +73,7 @@ async function letbet(bet, rul, valbet) {
                     console.log("[" + rul + "] " + body.bet.state + " - " + body.bet.amount + " - " + body.bet.profit + " | " + body.userBalance.amount);
                     //if (Math.abs(body.bet.profit) > 1) {
                     if (body.bet.profit < 0) {
-                        teledata = encodeURIComponent("[http://wolf.bet/id/casino/dice?betType=dice&id=" + body.bet.hash + "&modal=bet] " + body.bet.state + " - " + body.bet.amount + " - " + body.bet.profit + " | " + body.userBalance.amount)
+                        teledata = encodeURIComponent("[<a href=\"http://wolf.bet/id/casino/dice?betType=dice&id=" + body.bet.hash + "&modal=bet\">link</a>] " + body.bet.state + " - " + body.bet.amount + " - " + body.bet.profit + " | " + body.userBalance.amount)
                         resolve([body.bet.profit, teledata])
                     } else {
                         resolve([body.bet.profit, 0]);
